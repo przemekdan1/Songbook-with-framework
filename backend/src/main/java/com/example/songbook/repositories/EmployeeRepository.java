1badde0f1862a9cb96e36f4c3b25cd4346fc4d94
przemekdan1 <jange1337@interia.pl>
2024-05-12 07:27:13 +0200

    article examples

--- backend/src/main/java/com/example/songbook/repositories/EmployeeRepository.java
+++ backend/src/main/java/com/example/songbook/repositories/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.example.songbook.repositories;
 
 import com.example.songbook.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
+@RestResource
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
 }
