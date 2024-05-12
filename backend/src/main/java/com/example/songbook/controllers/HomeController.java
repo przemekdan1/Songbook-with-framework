a8126486f905bcffa0d513b8e664c30bd95404cd
przemekdan1 <jange1337@interia.pl>
2024-05-12 19:58:07 +0200

    add security dependencies, secure routing

--- backend/src/main/java/com/example/songbook/controllers/HomeController.java
+++ backend/src/main/java/com/example/songbook/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package com.example.songbook.controllers;
 
+import jakarta.persistence.PreRemove;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +11,12 @@ public class HomeController {
     @GetMapping("/home")
     public String home(){
         return "home";
+    }
+
+    @GetMapping("secure/path")
+    //@PreAuthorize("hasRole('ADMIN')")
+    public String secured(){
+        return "I'm secure";
     }
 }
 
