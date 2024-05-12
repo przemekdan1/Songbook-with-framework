868e566cccb0f8812cd9d95f237fa661e0624412
przemekdan1 <jange1337@interia.pl>
2024-05-12 15:42:52 +0200

    song model, repair project view

--- backend/src/main/java/com/example/songbook/models/Song.java
+++ backend/src/main/java/com/example/songbook/models/Song.java
@@ -1,8 +1,6 @@
 package com.example.songbook.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
@@ -10,9 +8,63 @@ import lombok.Data;
 @Table(name = "Song")
 public class Song {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String song_title;
     private String content;
     private long id_artist;
     private long id_category;
+
+    public Song() {
+    }
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getSong_title() {
+        return song_title;
+    }
+
+    public void setSong_title(String song_title) {
+        this.song_title = song_title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public long getId_artist() {
+        return id_artist;
+    }
+
+    public void setId_artist(long id_artist) {
+        this.id_artist = id_artist;
+    }
+
+    public long getId_category() {
+        return id_category;
+    }
+
+    public void setId_category(long id_category) {
+        this.id_category = id_category;
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "id=" + id +
+                ", song_title='" + song_title + '\'' +
+                ", content='" + content + '\'' +
+                ", id_artist=" + id_artist +
+                ", id_category=" + id_category +
+                '}';
+    }
 }
--- backend/src/main/java/com/example/songbook/repositories/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.songbook.repositories;
-
-import com.example.songbook.models.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RestResource;
-
-@RestResource
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-}
