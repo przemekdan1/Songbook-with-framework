336ae0f34885cd6183ecfa5b1a9b8826096fe266
przemekdan1 <jange1337@interia.pl>
2024-05-26 13:52:53 +0200

    feat(validation): add create user form validation

--- frontend/src/components/UserComponent.jsx
+++ frontend/src/components/UserComponent.jsx
@@ -1,7 +1,6 @@
 import React,{useState} from 'react';
 import {createUser} from "../services/UserService.js";
 import {useNavigate} from "react-router-dom";
-
 const UserComponent = () => {
 
     const [email, setEmail] = useState('');
@@ -10,18 +9,57 @@ const UserComponent = () => {
 
     const navigator = useNavigate();
 
+    const [errors, setErrors] = useState({
+        email:'',
+        username:'',
+        password:''
+    });
+
+    function validateForm(){
+        let valid = true;
+
+        const errorCopy = {...errors}
+
+        if(email.trim()){
+            errorCopy.email ='';
+        }else{
+            errorCopy.email ='Email required';
+            valid = false;
+        }
+
+        if(username.trim()){
+            errorCopy.username ='';
+        }else{
+            errorCopy.username ='Username required';
+            valid = false;
+        }
+
+        if(password.trim()){
+            errorCopy.password ='';
+        }else{
+            errorCopy.password ='Password required';
+            valid = false;
+        }
+
+        setErrors(errorCopy);
+        return valid;
+    }
+
 
     function saveUser(e){
         e.preventDefault();
 
-        const user = {username, email, password};
-        console.log(user)
+        if(validateForm()){
+            const user = {username, email, password};
+            console.log(user)
 
-        createUser(user).then((response) => {
-            console.log(response.data);
-            navigator('/users')
-        })
+            createUser(user).then((response) => {
+                console.log(response.data);
+                navigator('/users')
+            })
+        }
     }
+
 
 
     return (
@@ -38,11 +76,12 @@ const UserComponent = () => {
                                 <input
                                     type="text"
                                     id="username"
-                                    className="form-control"
+                                    className={`form-control ${errors.username ? 'is-invalid': ''}`}
                                     placeholder="Enter your username"
                                     value={username}
                                     onChange={(e) => setUsername(e.target.value)}
                                 />
+                                {errors.username && <div className='invalid-feedback'>{errors.username}</div>}
                             </div>
 
                             <div className='form-group'>
@@ -50,11 +89,12 @@ const UserComponent = () => {
                                 <input
                                     type="text"
                                     id="email"
-                                    className="form-control"
+                                    className={`form-control ${errors.email ? 'is-invalid': ''}`}
                                     placeholder="Enter your email"
                                     value={email}
                                     onChange={(e) => setEmail(e.target.value)}
                                 />
+                                {errors.email && <div className='invalid-feedback'>{errors.email}</div>}
                             </div>
 
                             <div className='form-group'>
@@ -62,14 +102,17 @@ const UserComponent = () => {
                                 <input
                                     type="password"
                                     id="password"
-                                    className="form-control"
+                                    className={`form-control ${errors.password ? 'is-invalid': ''}`}
                                     placeholder="**********"
                                     value={password}
                                     onChange={(e) => setPassword(e.target.value)}
                                 />
+                                {errors.password && <div className='invalid-feedback'>{errors.password}</div>}
                             </div>
                             <br/>
-                            <button className='btn btn-success' onClick={saveUser}>Submit</button>
+                            <button className='btn btn-success me-2' onClick={saveUser}>Submit</button>
+                            <button className='btn btn-danger' onClick={() => navigator('/users')}>Discard</button>
+
                         </form>
                     </div>
                 </div>
