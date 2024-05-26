30e0396f4d77683b4204bb6b200cf7528688eda4
przemekdan1 <jange1337@interia.pl>
2024-05-26 10:11:20 +0200

    feat(routing): add routing to user list

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import HelloWorld from "./components/HelloWorld.jsx";
 import UserListComponent from "./components/UserListComponent.jsx";
 import HeaderComponent from "./components/HeaderComponent.jsx";
 import FooterComponent from "./components/FooterComponent.jsx";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import HelloWorld from "./components/HelloWorld.jsx";
 
 function App() {
   return (
     <>
-        <HeaderComponent />
-        <UserListComponent />
-        <FooterComponent />
+        <BrowserRouter>
+            <HeaderComponent />
+                <Routes>
+                    <Route path='/' element={<HelloWorld/>} />
+                    <Route path='/users' element={<UserListComponent/>} />
+                </Routes>
+            <FooterComponent />
+        </BrowserRouter>
     </>
   )
 }
